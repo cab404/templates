30758a73ba376ff98fe5a6f6849bb7922e759d44
Vladimir Serov <me@cab404.ru>
2020-09-28 21:41:48 +0300

    Rust template based on Naersk

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,34 +1,27 @@
-let
-  packageName = "hello";
-  packageVersion = "0.1.0";
-  packageDescription = "CHANGEME";
-in
 {
 
-  description = packageDescription;
+  description = "CHANGEME";
 
   inputs = {
+    naersk.url = "github:nmattia/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk {};
       in {
 
-        defaultPackage = with pkgs;
-          rustPlatform.buildRustPackage {
-            pname = packageName;
-            version = packageVersion;
-            src = ./.;
-            cargoSha256 = "0gdpfzs62hph65yzbf8mm0xfmvihsprigz7jq4jfxh08yf0w7s1i";
-          };
+        defaultPackage = naersk-lib.buildPackage ./.;
 
         defaultApp = {
           type = "app";
-          program = "${self.defaultPackage."${system}"}/bin/wgbond";
+          program = "${self.defaultPackage."${system}"}/bin/${self.defaultPackage."${system}".pname}";
         };
 
       });
+
 }
